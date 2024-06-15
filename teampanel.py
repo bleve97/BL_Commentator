98eb63a2a5d758276577283415b1659f3a5d2d11
Carl Brewer <carl@aboc.net.au>
2024-06-15 17:02:18 +1000

    starting on player info boxes in teampanel.py

--- teampanel.py
+++ teampanel.py
@@ -5,19 +5,40 @@ import includes
 import commclasses as CC
 import scoreboard as SB
 
-class TeamFrame(wx.Frame):
 
-    def __init__(self, title, parent=None):
-        wx.Frame.__init__(self, parent=parent, title=title)
-        self.Show()
+
+class PlayerInfoPanel(wx.Panel):
+
+    def __init__(self, parent, player):
+        super().__init__(parent)
+        box = wx.StaticBox(self, -1, "Player staticbox")
+        bsizer = wx.StaticBoxSizer(box, wx.VERTICAL)
+
+        t = wx.StaticText(self, -1, "hi there")
+        bsizer.Add(t,0,wx.TOP|wx.LEFT, 10)
+
+        border = wx.BoxSizer()
+        border.Add(bsizer, 1, wx.EXPAND|wx.ALL, 25)
+        self.SetSizer(border)
+
+
+
 
 
 class TeamPanel(wx.Panel):
+
     def __init__(self, parent):
         super().__init__(parent)
         main_sizer = wx.BoxSizer(wx.VERTICAL)
-class PlayerInfoPanel():
-    def __init__(self):
-        print("hi")
+#class PlayerInfoPanel():
+#    def __init__(self):
+#        print("hi")
     # hi
 
+
+class TeamFrame(wx.Frame):
+
+    def __init__(self, title, parent=None):
+        wx.Frame.__init__(self,parent=parent, title=title)
+        panel=PlayerInfoPanel(self,player="foo")
+        self.Show()
