0c63502f6c20db7b599caa1872d97fc3ee45277d
Carl Brewer <carl@aboc.net.au>
2024-06-29 17:44:13 +1000

    no changes, g6 WPRO

--- includes.py
+++ includes.py
@@ -6,7 +6,7 @@ import time, datetime
 AppName = "BL Commentator"
 Version = "0.1a1"
 
-defaultGameID = 6565
+defaultGameID = 6553
 
 # LastMinSkip = False     # there's an issue at iceHQ with network access to the scoreboard
 # if this is set to True, the scoreboard refresh routine will skip polling the file at 61s to go.
